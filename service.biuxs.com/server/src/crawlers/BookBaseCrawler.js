353f5b0e878693a6332d1e07e52259f7741492e7
zhouliujun <1096432931@qq.com>
2020-08-07 17:54:13 +0800

    更新

--- service.biuxs.com/server/src/crawlers/BookBaseCrawler.js
+++ service.biuxs.com/server/src/crawlers/BookBaseCrawler.js
@@ -2,7 +2,8 @@
  * 书籍的基本信息采集爬虫
  */
 const http = require('superagent');
-const tr = require('transliteration'); //分类拼音
+// const tr = require('transliteration'); //分类拼音
+const { logger } = require(':lib/logger4'); //日志系统
 // const redis = require(':lib/redis'); //redis
 const utils = require(':lib/Utils');
 require('superagent-proxy')(http);
@@ -130,13 +131,13 @@ module.exports = class BookBaseCrawler {
 
             //创建异步同时抓取多个分类
             const crawlers = crawlerConfig.types.map((config) => {
-                return this.getBookTypeBase(crawlerConfig.base, config);
+                return this.getBookTypeBase(crawlerConfig.base, config, config.page);
             });
             Promise.all(crawlers).then((res) => {
                 console.log(`抓取完成!`, res);
             });
         } catch (error) {
-            console.log(`抓取错误!`, error);
+            logger.error(`抓取站点的分类信息出错:`, JSON.stringify(error));
             return error;
         }
     }
@@ -149,7 +150,7 @@ module.exports = class BookBaseCrawler {
     */
     async getBookTypeBase(baseConf, config, page = 1) {
         const headers = this.__getRequestHeaders(baseConf.host); //获取请求头
-        const params = config.params.replace('[page]', config.page); //替换url中的分页参数
+        const params = config.params.replace('[page]', page); //替换url中的分页参数
         const baseURL = `${baseConf.protocol}${baseConf.host}${params}`;
         try {
             console.info(`============================================抓取开始 ${baseURL}-${baseConf.title}-${config.title} BEGIN================================================`);
@@ -159,18 +160,17 @@ module.exports = class BookBaseCrawler {
                 const $ = cheerio.load(text, { decodeEntities: false }); //decodeEntities 设置了某些站点不会出现乱码
                 $(baseConf.listSelector).find(baseConf.itemSelector).each((index, el) => {
                     list.push({
-                        title: config.title, //分类标题
-                        pingyin: tr.slugify(config.title).replace(/-/g, ''), //拼音
+                        // title: config.title, //分类标题
+                        // pingyin: tr.slugify(config.title).replace(/-/g, ''), //拼音
                         website: baseConf.title, //站点名称
                         domain: `${baseConf.protocol}${baseConf.host}`, //域名地址
-                        name: `抓取[baseConf.title]-[${config.title}]-[${$(el).find(config.name).text().trim()}]`, //任务名称
+                        name: `抓取[${baseConf.title}]-[${config.title}]-[${$(el).find(baseConf.name).text().trim()}]`, //任务名称
                         url: `${baseConf.protocol}${baseConf.host}${$(el).find(baseConf.href).attr('href').trim()}`, //获取采集地址链接
                         type: 1, //任务采集类型 1分类 2书籍 3章节 4内容
                         status: 1 //状态(1未开始 2进行中 3未完成 4已完成)
                     });
                 });
             }
-            console.info(`=============================================抓取结束 ${baseURL}-${baseConf.title}-${config.title} END=================================================`);
             if (list.length) {
                 console.log(list);
                 await this.saveData(config, list);
@@ -179,11 +179,14 @@ module.exports = class BookBaseCrawler {
                 const time = utils.getRandomNum(500, 2000);
                 console.log(`抓取间隔 ${time} ms`);
                 setTimeout(() => {
-                    this.getBookTypeBase(config, page);
+                    this.getBookTypeBase(baseConf, config, page);
                 }, time);
             }
+            console.info(`=============================================抓取结束 ${baseURL}-${baseConf.title}-${config.title} END=================================================`);
+            return list;
         } catch (error) {
-            console.log(`${baseURL}-${config.title}抓取错误!`, error);
+            logger.error(`${baseURL}-${baseConf.title}-${config.title}抓取错误!`, JSON.stringify(error));
+            console.log(`${baseURL}-${baseConf.title}-${config.title}抓取错误!`, error);
             return null;
         }
     }
@@ -195,7 +198,7 @@ module.exports = class BookBaseCrawler {
     async saveData(config, list) {
         try {
             const datas = await TasksModel.findAll({
-                where: { title: config.title, status: 1, isDelete: false }
+                where: { status: 1, isDelete: false }
             });
             if (datas && datas.length) { //查询数据库已有的数据
                 for (let i in datas) {
